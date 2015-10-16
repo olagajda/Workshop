cc4837a938916097956ebcedf34d5716e9b2ea93
olagajda <aleksandra.gajda@gmail.com>
2015-10-16 14:52:35 +0200

    Added dropdown menu mini-project

---
+++ dropdown-menu/js/app.js
@@ -0,0 +1,28 @@
+/**
+ * Created by aleksandragajda on 16.10.15.
+ */
+
+$(function(){
+
+
+    var menuButton = $(".list p");
+
+    menuButton.on("click", function(event){
+
+        $(this).next().children().slideToggle(500);
+
+
+
+
+
+
+    });
+
+
+
+
+
+
+
+
+});
