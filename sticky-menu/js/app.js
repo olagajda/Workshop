209ae2a7678abb31f8d162c00c564a01874cd002
olagajda <aleksandra.gajda@gmail.com>
2015-10-16 11:08:52 +0200

    Added animation effect to sticky menu

--- sticky-menu/js/app.js
+++ sticky-menu/js/app.js
@@ -6,14 +6,25 @@ $(function(){
 
     var menu = $("#mainMenu");
     var lastPositionTop = 0;
+    var menuHeight = menu.height();
 
     $(window).scroll(function(event){
 
         if (menu.hasClass("sticky")===false && (menu.offset().top) < ($(this).scrollTop())){
 
 
+            //scrollTop zwraca nam odległość od góry strony
+            //offset zwraca odległość od elementu od góry strony
+
+
             lastPositionTop = menu.offset().top;
             menu.addClass("sticky");
+
+            menu.animate({
+                height: (menuHeight/3).toString() +"px"
+            }, 300)
+
+
 
 
 
@@ -22,6 +33,10 @@ $(function(){
         if (menu.hasClass("sticky") && (lastPositionTop > ($(this).scrollTop()))) {
 
             menu.removeClass("sticky");
+            menu.animate({
+                height: (menuHeight).toString() +"px"
+
+            }, 300)
 
         }
 
